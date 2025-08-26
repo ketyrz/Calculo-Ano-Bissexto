b623a85c4f97a011cab215b61fb55c0f4e456e2a
Gabrielkeppen <83722143+Gabrielkeppen@users.noreply.github.com>
2025-08-25 21:19:16 -0300

    adicionando regra de formato

--- ano_bissexto.py
+++ ano_bissexto.py
@@ -1,6 +1,11 @@
-ano = int(input("Digite um ano entre 2000 e 4000 : "))
-if ano > 2000 and ano < 4000:
-    bissexto = (ano - 2000) // 4
-    print("Desde o ano 2000 até o ano ",ano ," tiveram ", bissexto, " anos bissextos")
-else :
+try:
+    ano = int(input("Digite um ano entre 2000 e 4000 : "))
+
+    if ano > 2000 and ano < 4000:
+        bissexto = (ano - 2000) // 4
+        print("Desde o ano 2000 até o ano ",ano ," tiveram ", bissexto, " anos bissextos")
+    else :
+        print("Ano inválido")
+
+except:
     print("Ano inválido")
