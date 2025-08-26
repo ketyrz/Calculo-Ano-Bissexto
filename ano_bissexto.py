d4146539b15b1eab610e046b2593102386dbffa2
Gabrielkeppen <83722143+Gabrielkeppen@users.noreply.github.com>
2025-08-25 21:00:24 -0300

    removendo laço while

--- ano_bissexto.py
+++ ano_bissexto.py
@@ -1,12 +1,6 @@
-i = 0
-
-while i <= 2:
-    i = i + 1
-
-    ano = int(input("Digite um ano entre 2000 e 4000 : "))
-    if ano > 2000 and ano < 4000:
-        bissexto = (ano - 2000) // 4
-        print("Desde o ano 2000 até o ano ",ano ," tiveram ", bissexto, " anos bissextos")
-        break
-    else :
-        print("Ano inválido")
+ano = int(input("Digite um ano entre 2000 e 4000 : "))
+if ano > 2000 and ano < 4000:
+    bissexto = (ano - 2000) // 4
+    print("Desde o ano 2000 até o ano ",ano ," tiveram ", bissexto, " anos bissextos")
+else :
+    print("Ano inválido")
